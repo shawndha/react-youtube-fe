15314a849fd266daa27d07153773026031e1c0cb
Shawn Dhaliwal <shawndhaliwal_28@hotmail.com>
2018-11-10 21:39:11 -0800

    Default static search term and play capability on first search result

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search-bar'
-
+import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
 const API_KEY = 'AIzaSyA1ZwYnL0kMqZHieHoKi_4Az7_XXOWwM5I'
 
-// Create a new component. This component should produce some HTML
-const App = () =>  {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	)
+class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {videos: []}
+
+		YTSearch({ key: API_KEY, term:'surfboards' }, (videos) => {
+			this.setState({videos})
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video = {this.state.videos[0]}/>
+				<VideoList videos = {this.state.videos}/>
+			</div>
+		)
+	}
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'))
