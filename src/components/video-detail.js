a3002efcfdf469b4a0f149aaed543529c7c62197
Shawn Dhaliwal <shawndhaliwal_28@hotmail.com>
2018-11-10 22:38:36 -0800

    Video search and select functionality and basic styling

--- src/components/video-detail.js
+++ src/components/video-detail.js
@@ -5,16 +5,16 @@ const VideoDetail = ({video}) => {
 		return <div>Loading...</div>
 	}
 	const videoId = video.id.videoId
-	console.log(videoId)
+	console.log("current video ", video)
 	const url = `https://www.youtube.com/embed/${videoId}`
 
 	return (
 		<div className="video-detail col-md-8">
 			<div className="embed-responsive embed-responsive-16by9">
-				<iframe className="embed-responsive-item" src={url}></iframe>
+				<iframe className="embed-responsive-item" src={url} allowFullScreen></iframe>
 			</div>
 			<div className="details">
-				<div>{video.snippet.title}</div>
+				<div className="selectedVideoTitle">{video.snippet.title}</div>
 				<div>{video.snippet.description}</div>
 			</div>
 		</div>
