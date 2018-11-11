04dd4b6295fd0de878dd86bf17ea87577bfe4b36
Shawn Dhaliwal <shawndhaliwal_28@hotmail.com>
2018-11-10 18:02:12 -0800

    Basic detection of user input in search-bar

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
 	render() {
-		return <input />
+		return <input onChange = {event => console.log(event.target.value)}/>
 	}
+
 }
 
 export default SearchBar
